ac30e5f119b0c72378b5122cff83f4bc56d9dc3f
ZoneZonekiddokiddo <165952022+ZoneZonekiddokiddo@users.noreply.github.com>
2024-04-03 14:33:12 -0500

    Add files via upload

---
+++ Servo_Nonsense.ino
@@ -0,0 +1,27 @@
+#include <Servo.h>
+
+
+const int buttonPin = [insert switch here];
+const int switchPin = 13;
+Servo myservo;
+int servoAngle = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
+  myservo.attach(switchPin);
+  myservo.write(servoAngle);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == HIGH) {
+    if (servoAngle < 180) {
+      servoAngle += servoSpeed;
+    }
+  } else {
+    servoAngle = 0;
+  }
+
+  Serial.println(servoAngle);
+
+}
