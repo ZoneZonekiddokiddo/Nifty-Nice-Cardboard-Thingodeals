8549a67f1a883e8338f2d16049421d191ca9235c
mr0per <165730842+mr0per@users.noreply.github.com>
2024-04-05 10:13:09 -0500

    Update psuedocodo.ino
    added other servos + switch code

--- psuedocodo.ino
+++ psuedocodo.ino
@@ -1,5 +1,8 @@
 #include <Servo.h>
-const int switchPin = 3;
+const int switchPinA = 2;
+const int switchPinB = 3;
+const int switchPinC = 4;
+const int switchPinD = 5;
 int switchValA = 0;
 int prevswitchValA = 0;
 Servo myservoA;
@@ -10,7 +13,10 @@ Servo myservoD;
 void setup() 
 {
   // put your setup code here, to run once:
-  pinMode(switchPin, INPUT);
+  pinMode(switchPinA, INPUT);
+  pinMode(switchPinB, INPUT);
+  pinMode(switchPinC, INPUT);
+  pinMode(switchPinD, INPUT);
   myservoA.attach(10);
   myservoB.attach(11);
   myservoC.attach(12);
@@ -24,7 +30,7 @@ void setup()
 
 void loop()
 {
-   switchValA = digitalRead(switchPin);
+   switchValA = digitalRead(switchPinA);
 
   if(switchValA != prevswitchValA)     // servos will only run if switch is clicked 
   {
@@ -39,4 +45,66 @@ void loop()
     servoA.write(0);
   }
   prevswitchValA = switchValA;
-}
+
+
+  
+
+  switchValB = digitalRead(switchPinB);
+
+  if(switchValB != prevswitchValB)     // servos will only run if switch is clicked 
+  {
+    if (switchValB == HIGH)
+    {
+      myservoB.write(180);
+    }
+    
+  }
+  else 
+  {
+    myservoB.write(0);
+  }
+  prevswitchValB = switchValB;
+
+
+
+
+switchValC = digitalRead(switchPinC);
+
+  if(switchValC != prevswitchValC)     // servos will only run if switch is clicked 
+  {
+    if (switchValC == HIGH)
+    {
+      myservoC.write(180);
+    }
+    
+  }
+  else 
+  {
+    servoC.write(0);
+  }
+  prevswitchValC = switchValC;
+
+
+
+
+
+
+switchValD = digitalRead(switchPinD);
+
+  if(switchValD != prevswitchValD)     // servos will only run if switch is clicked 
+  {
+    if (switchValD == HIGH)
+    {
+      myservoD.write(180);
+    }
+    
+  }
+  else 
+  {
+    servoD.write(0);
+  }
+  prevswitchValD = switchValD;
+
+
+
+
