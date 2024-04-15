c40b7cda9d36b95ca4c15c397f0abd01d98d65d3
mr0per <165730842+mr0per@users.noreply.github.com>
2024-04-14 22:37:40 -0500

    Update StoryCrafters_Enclosure_Code.ino

--- StoryCrafters_Enclosure_Code.ino
+++ StoryCrafters_Enclosure_Code.ino
@@ -5,9 +5,12 @@
 #include <Servo.h>
 
 //---------------------------------------------------interaction 1 set up------------------
-const int switchPinA = 12;
-int pos = 0;
-Servo myservoA; 
+const int switchPinA = 8;
+Servo myservoA;  
+int pos = 90;   
+unsigned long previousMillis = 0;  
+const long interval = 50;  
+int increment = -1;  
 //----------------------------------------------------------------------------------------
 
 
@@ -21,7 +24,12 @@ Servo myservoB;
 
 
 //---------------------------------------------------interaction 4 set up------------------
-
+const int switchPinC = 8;
+Servo myservoA;  
+int pos = 90;   
+unsigned long previousMillis = 0;  
+const long interval = 50;  
+int increment = -1;  
 //------------------------------------------------------------------------------------------
 
 
@@ -36,7 +44,7 @@ void setup() {
 //---------------------------------------------- interaction 1-------------------------------------------
   pinMode(switchPinA, INPUT);
   myservoA.attach(4);  
-  myservoA.write(0);
+
 //--------------------------------------------------------------------------------------------------------
 
 //---------------------------------------------- interaction 2-------------------------------------------
@@ -46,7 +54,8 @@ void setup() {
 //--------------------------------------------------------------------------------------------------------
 
 //---------------------------------------------- interaction 4-------------------------------------------
-  
+   pinMode(switchPinA, INPUT);
+  myservoA.attach(7);  
 //--------------------------------------------------------------------------------------------------------
 
 //---------------------------------------------- interaction 5-------------------------------------------
@@ -62,24 +71,23 @@ void loop() {
 
 //--------------------------------------------Interaction 1--------------------------------------------------------
 // when the switch is pressed down the servo will "wiggle" back and forth from 180 to 0 degrees and when the switch is not being pressed the servo will be at 0 degrees
- if (digitalRead(switchPinA) == HIGH)
-  {
- 
-    for (pos = 0; pos <= 180; pos += 1)
-    { 
-      myservoA.write(pos);
+unsigned long currentMillis = millis();  
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis; 
+    if (digitalRead(switchPinA) == HIGH) {
+      myservoA.write(pos);  // tell servo to go to position in variable 'pos'
+      pos += increment;  // increment or decrement position based on the direction
+
+      // If the position reaches 0 or 180, change the direction of movement
+      if (pos <= 0 || pos >= 180) {
+        increment = -increment;  // reverse the direction
+      }
+    } else {
+      myservoA.write(0);
     }
-    for (pos = 180; pos >= 0; pos -= 1) {
-    myservoA.write(pos);  
-           
-                    
   }
-                
-  }
-  else
-  {
-    myservoA.write(0);
-  }
+  
 //------------------------------------------------------------------------------------------------------------------------
 
 
@@ -110,28 +118,28 @@ void loop() {
 
 
 
-  //------------------------------------------------- Interaction 3--------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-  //---------------------------------------------------------------------------------------------------------------------------------
 
 
    //------------------------------------------------- Interaction 4--------------------------------------------------------------
 
 
+unsigned long currentMillis = millis();  
 
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis; 
+    if (digitalRead(switchPinA) == HIGH) {
+      myservoC.write(pos);  // tell servo to go to position in variable 'pos'
+      pos += increment;  // increment or decrement position based on the direction
 
-
-
-
-
+      // If the position reaches 0 or 180, change the direction of movement
+      if (pos <= 0 || pos >= 180) {
+        increment = -increment;  // reverse the direction
+      }
+    } else {
+      myservoC.write(0);
+    }
+  }
 
   //---------------------------------------------------------------------------------------------------------------------------------
 
